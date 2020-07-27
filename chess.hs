d97e2df6f638886ce8a13f03f164322349aee5f6
VictorKeil <keilvictor60@gmail.com>
2020-07-28 01:44:30 +0300

    Rework move validation, add Rook rules, instance Show Board
    The previous two step move validation was overly complicated and was
    therefore abolished. Instead of generating a predicate list to be
    checked by some other function, the validMoves function is now
    directly called with the relevant piece and the board. Each piece is
    matched against the correct function. The Show implementation for
    Board is not very robust, and should probably be cleaned up at some
    point.

--- chess.hs
+++ chess.hs
@@ -13,48 +13,122 @@ instance Hashable Square where
 
 data Color = Black | White deriving (Show, Eq)
 
-data PieceType = Pawn deriving (Show)
+data PieceType = Pawn | Rook | Knight deriving (Show)
 
 data Piece = Piece PieceType Color Square deriving (Show)
 
 type MovePredicates = (Square, [Maybe Piece -> Bool])
 type MoveList = [(Square, [MovePredicates])]
 
-type Board = HMap.Map Square Piece
+data Bounds = Bounds {minX :: Int, minY :: Int, maxX :: Int, maxY :: Int} deriving (Show)
 
-genBoard :: Board
-genBoard = let whitePawns = [(Square x 6, Piece Pawn White (Square x 6)) | x <- [1..8]]
-               blackPawns = [(Square x 7, Piece Pawn Black (Square x 7)) | x <- [1..8]]
-           in HMap.fromList (whitePawns ++ blackPawns)
+square :: Piece -> Square
+square (Piece _ _ sqr) = sqr
 
-checkMovePreds :: Board -> MovePredicates -> Bool
-checkMovePreds b (s, fs) = and $ map ($ (HMap.lookup s b)) fs
+pieceSym :: Piece -> Char
+pieceSym (Piece Knight _ _) = 'N'
+pieceSym (Piece kind _ _) = head . show $ kind
 
-globalValidMoves :: Board -> MoveList -> [Square]
-globalValidMoves b ml = map fst $ filter (and . (map $ checkMovePreds b) . snd) ml
+type BoardMap = HMap.Map Square Piece
 
-sameColor :: Color -> Maybe Piece -> Bool
-sameColor _ Nothing = False
-sameColor col1 (Just (Piece _ col2 _)) = col1 == col2
+data Board = Board BoardMap Bounds
+  
+lookupS :: Board -> Square -> Maybe Piece
+lookupS (Board brd _) sqr = HMap.lookup sqr brd
 
-preValidMoves :: Piece -> MoveList
-preValidMoves (Piece Pawn color (Square col row)) =
-  let (direction, initRow) = if color == White then (1, 2) else (-1, 7)
-      basePredicates = [null]
-      baseSquares = 
+instance Show Board where
+  show (Board brd bnds) = let width = maxX bnds - minX bnds + 1
+                              height = maxY bnds - minY bnds + 1
+
+                              -- Make row and column coordinate axes
+                              cols = ' ':[toEnum (x + 64) :: Char | x <- [1 .. width]]
+                              rows = [head . show $ x | x <- [height, height - 1 .. 1]]
+  
+                              -- Generate a list of strings that alternate between '.' and 'O'
+                              -- Each string represents a row on the chess board
+                              textSquares =
+                                [[if odd (x+y) then '.' else 'O' | x <- [1 .. width]] | y <- [1 .. height]]
+                              -- Generate a list with similar structure as the one above, but fill it with
+                              -- corresponding Square objects
+                              squares = reverse [[(Square x y) | x <- [1 .. width]] | y <- [1 .. height]]
+                              -- Zip the previous two lists together to create a list of list of pairs of
+                              -- empty square characters and Square objects
+                              textSqrPairs = zipWith zip textSquares squares
+                              -- Map through each sublist, look for a piece on the board on each square.
+                              -- If a piece is found, replace that character with the pieceSym of the piece.
+                              -- If a piece is not found, simply return the empty square character of that square.
+                              filledBoard =
+                                (flip map) textSqrPairs (\pairList ->
+                                                           (flip map) pairList (\pair -> 
+                                                                                  case HMap.lookup (snd pair) brd of
+                                                                                    Nothing -> fst pair
+                                                                                    Just piece -> pieceSym piece))
+
+                              -- Add row and column coordinate axes to board
+                              finalBoard = (transpose $ rows:(transpose filledBoard)) ++ [cols]
+                          -- Before returning the board, add a space between every square and unline it
+                          in unlines $ map (intersperse ' ') finalBoard
+
+
+sameColor :: BoardMap -> Color -> Square -> Bool
+sameColor brd col1 sqr = case HMap.lookup sqr brd of
+                          Nothing -> False
+                          Just (Piece _ col2 _) -> col1 == col2
+
+nullSquare :: (HMap.Map Square Piece) -> Square -> Bool
+nullSquare brd = null . (flip HMap.lookup $ brd)
+
+-- Like takeWhile, but includes the first element for which the predicate is false
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil _ [] = []
+takeUntil f (x:xs) = if f x then x:(takeUntil f xs) else [x]
+
+-- Take mechanics for long range pieces
+takeLong :: BoardMap -> Color -> [Square] -> [Square]
+takeLong brd color sqrs = case takeUntil (nullSquare brd) sqrs of
+                            [] -> []
+                            xs -> takeWhile (not . sameColor brd color) xs
+                          
+
+validMoves :: Board -> Piece -> [Square]
+validMoves (Board brd bnds) (Piece Pawn color (Square col row)) =
+  let (direction, initRow) = if color == White then (1,2) else (-1, 7)
+      baseSquares =
         if row == initRow
         then [Square col (row + direction * x) | x <- [1..2]]
         else [Square col (row + direction)]
-      baseMoves = [(last xs, [(x, basePredicates) | x <- xs]) | xs <- tail . inits $ baseSquares]
-
-      capturePredicates = [not . null, not . sameColor color]
+      baseMoves = takeWhile (nullSquare brd) baseSquares
+      
       captureSquares =
         [Square (col - 1) (row + direction), Square (col + 1) (row + direction)]
-      captureMoves = map (\s -> (s, [(s, capturePredicates)])) captureSquares
-     in baseMoves ++ captureMoves
+      captureMoves = filter (not . sameColor brd color ) captureSquares
+  in captureSquares
+  
+validMoves (Board brd bnds) (Piece Rook color (Square col row)) =
+  let posX = [Square (col + x) row | x <- [1..maxX bnds - col]]
+      negX = [Square (col - x) row | x <- [1..col - minX bnds]]
+      posY = [Square col (row + y) | y <- [1..maxY bnds - row]]
+      negY = [Square col (row - y) | y <- [1..row - minY bnds]]
 
+      -- validX = concat $ map (takeWhile $ nullSquare brd) $ [negX, posX, negY, posY]
+      validX = concat $ map (takeLong brd color) $ [negX, posX, negY, posY]
+  in validX
 
-board = genBoard
-p1 = Piece Pawn White (Square 5 6)
+strSqr :: String -> Square
+strSqr (a:n:rs) = Square (fromEnum a - 64) (fromEnum n - 48)
+
+genBoard :: Bounds -> Board
+genBoard bnds = let whitePawns = [(Square x 2, Piece Pawn White (Square x 2)) | x <- [1..8]]
+                    blackPawns = [(Square x 7, Piece Pawn Black (Square x 7)) | x <- [1..8]]
+                    rook = [(Square 5 5, Piece Rook White (Square 5 5))]
+                in Board (HMap.fromList (whitePawns ++ blackPawns ++ rook)) bnds
+
+moves :: Board -> Square -> [Square]
+moves brd sqr = case lookupS brd sqr of Nothing -> []
+                                        Just p -> validMoves brd p
+
+board = genBoard (Bounds 1 1 8 8)
+p1 = Piece Pawn White (Square 5 2)
 p2 = Piece Pawn Black (Square 5 7)
+r1 = Piece Rook White (Square 5 4)
 
