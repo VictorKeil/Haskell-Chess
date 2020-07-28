82cbc3d2b8b0f387964160d0459fdd10210a2b1b
VictorKeil <keilvictor60@gmail.com>
2020-07-28 23:47:07 +0300

    Change valid square finding process, impelment Bishop
    Squares are now calculated with a two step process. The first pass
    evaluates all squares that a piece targets. This is useful for
    determining whether or not the king is allowed to move to a given
    square or not. The second pass filters out the targeted squares that
    are not valid moves. Pawn movement is an edge case, and has a modified
    second pass.

--- chess.hs
+++ chess.hs
@@ -13,7 +13,7 @@ instance Hashable Square where
 
 data Color = Black | White deriving (Show, Eq)
 
-data PieceType = Pawn | Rook | Knight deriving (Show)
+data PieceType = Pawn | Rook | Bishop | Knight deriving (Show)
 
 data Piece = Piece PieceType Color Square deriving (Show)
 
@@ -27,7 +27,7 @@ square (Piece _ _ sqr) = sqr
 
 pieceSym :: Piece -> Char
 pieceSym (Piece Knight _ _) = 'N'
-pieceSym (Piece kind _ _) = head . show $ kind
+pieceSym (Piece kind _ _) = head $ show kind
 
 type BoardMap = HMap.Map Square Piece
 
@@ -44,10 +44,10 @@ instance Show Board where
                               cols = ' ':[toEnum (x + 64) :: Char | x <- [1 .. width]]
                               rows = [head . show $ x | x <- [height, height - 1 .. 1]]
   
-                              -- Generate a list of strings that alternate between '.' and 'O'
+                              -- Generate a list of strings that alternate between '.' and '#'
                               -- Each string represents a row on the chess board
                               textSquares =
-                                [[if odd (x+y) then '.' else 'O' | x <- [1 .. width]] | y <- [1 .. height]]
+                                [[if odd (x+y) then '.' else '#' | x <- [1 .. width]] | y <- [1 .. height]]
                               -- Generate a list with similar structure as the one above, but fill it with
                               -- corresponding Square objects
                               squares = reverse [[(Square x y) | x <- [1 .. width]] | y <- [1 .. height]]
@@ -83,12 +83,33 @@ takeUntil :: (a -> Bool) -> [a] -> [a]
 takeUntil _ [] = []
 takeUntil f (x:xs) = if f x then x:(takeUntil f xs) else [x]
 
--- Take mechanics for long range pieces
-takeLong :: BoardMap -> Color -> [Square] -> [Square]
-takeLong brd color sqrs = case takeUntil (nullSquare brd) sqrs of
-                            [] -> []
-                            xs -> takeWhile (not . sameColor brd color) xs
-                          
+-- Move mechanics for long range pieces
+takeLong :: BoardMap -> [Square] -> [Square]
+takeLong brd = takeUntil (nullSquare brd)
+
+targetSquares :: Board -> Piece -> [Square]
+targetSquares (Board brd bnds) (Piece Pawn color (Square col row)) =
+  let (direction, initRow) = if color == White then (1,2) else (-1, 7)
+  in [Square (col - 1) (row + direction), Square (col + 1) (row + direction)]
+  
+targetSquares (Board brd bnds) (Piece Rook color (Square col row)) =
+  let posX = [Square (col + x) row | x <- [1..maxX bnds - col]]
+      negX = [Square (col - x) row | x <- [1..col - minX bnds]]
+      posY = [Square col (row + y) | y <- [1..maxY bnds - row]]
+      negY = [Square col (row - y) | y <- [1..row - minY bnds]]
+  in concat $ map (takeLong brd) $ [negX, posX, negY, posY]
+
+targetSquares (Board brd bnds) (Piece Bishop color (Square col row)) =
+  let maxNorth = maxY bnds - row
+      maxSouth = row - minY bnds
+      maxWest = col - minX bnds
+      maxEast = maxX bnds - col
+      
+      ne = [Square (col + x) (row + x) | x <- [1 .. max maxNorth maxEast]]
+      se = [Square (col + x) (row - x) | x <- [1 .. max maxSouth maxEast]]
+      sw = [Square (col - x) (row - x) | x <- [1 .. max maxSouth maxWest]]
+      nw = [Square (col - x) (row + x) | x <- [1 .. max maxNorth maxWest]]
+  in concat $ map (takeLong brd) $ [ne, se, sw, nw]
 
 validMoves :: Board -> Piece -> [Square]
 validMoves (Board brd bnds) (Piece Pawn color (Square col row)) =
@@ -99,20 +120,12 @@ validMoves (Board brd bnds) (Piece Pawn color (Square col row)) =
         else [Square col (row + direction)]
       baseMoves = takeWhile (nullSquare brd) baseSquares
       
-      captureSquares =
-        [Square (col - 1) (row + direction), Square (col + 1) (row + direction)]
-      captureMoves = filter (not . sameColor brd color ) captureSquares
-  in captureSquares
+      tgtSqrs = targetSquares (Board brd bnds) (Piece Pawn color (Square col row))
+      validCaptures = filter (\s -> not (sameColor brd color s || nullSquare brd s)) tgtSqrs
+  in validCaptures ++ baseMoves
   
-validMoves (Board brd bnds) (Piece Rook color (Square col row)) =
-  let posX = [Square (col + x) row | x <- [1..maxX bnds - col]]
-      negX = [Square (col - x) row | x <- [1..col - minX bnds]]
-      posY = [Square col (row + y) | y <- [1..maxY bnds - row]]
-      negY = [Square col (row - y) | y <- [1..row - minY bnds]]
-
-      -- validX = concat $ map (takeWhile $ nullSquare brd) $ [negX, posX, negY, posY]
-      validX = concat $ map (takeLong brd color) $ [negX, posX, negY, posY]
-  in validX
+validMoves (Board brd bnds) (Piece ptype color sqr) =
+  filter (not . sameColor brd color) $ targetSquares (Board brd bnds) (Piece ptype color sqr)
 
 strSqr :: String -> Square
 strSqr (a:n:rs) = Square (fromEnum a - 64) (fromEnum n - 48)
@@ -120,8 +133,8 @@ strSqr (a:n:rs) = Square (fromEnum a - 64) (fromEnum n - 48)
 genBoard :: Bounds -> Board
 genBoard bnds = let whitePawns = [(Square x 2, Piece Pawn White (Square x 2)) | x <- [1..8]]
                     blackPawns = [(Square x 7, Piece Pawn Black (Square x 7)) | x <- [1..8]]
-                    rook = [(Square 5 5, Piece Rook White (Square 5 5))]
-                in Board (HMap.fromList (whitePawns ++ blackPawns ++ rook)) bnds
+                    -- rook = [(Square 5 5, Piece Bishop White (Square 5 5))]
+                in Board (HMap.fromList (whitePawns ++ blackPawns)) bnds
 
 moves :: Board -> Square -> [Square]
 moves brd sqr = case lookupS brd sqr of Nothing -> []
@@ -131,4 +144,5 @@ board = genBoard (Bounds 1 1 8 8)
 p1 = Piece Pawn White (Square 5 2)
 p2 = Piece Pawn Black (Square 5 7)
 r1 = Piece Rook White (Square 5 4)
+b1 = Piece Bishop White (Square 5 5)
 
